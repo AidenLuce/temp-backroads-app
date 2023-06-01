cbfb476cca9b10e4f5a3c54b92fb03dd277ed45e
Aiden Luce <luceaiden4124@gmail.com>
2023-06-01 13:15:31 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,108 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
+import tour5 from './images/tour-5.jpeg'
+import tour6 from './images/tour-6.jpeg'
+
+
+export const pageLinks = [
+    {id: 1, href: '#home', text: 'home'},
+    {id: 2, href: '#about', text: 'about'},
+    {id: 3, href: '#services', text: 'services'},
+    {id: 4, href: '#tours', text: 'tours'},
+]
+export const socialLinks = [
+    {id:1, href: 'https://www.twitter.com', icon: 'fab fa-facebook'},
+    {id:2, href: 'https://www.twitter.com', icon: 'fab fa-twitter'},
+    {id:3, href: 'https://www.twitter.com', icon: 'fab fa-squarespace'},
+]
+export const services =[
+    {
+        id: 1,
+        icon: 'fas fa-wallet fa-fw',
+        title: 'saving money',
+        text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.'
+    },
+    {
+        id: 2,
+        icon: 'fas fa-tree fa-fw',
+        title: 'endless hiking',
+        text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.'
+    },
+    {
+        id: 3,
+        icon: 'fas fa-socks fa-fw',
+        title: 'amazing comfort',
+        text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.'
+    },
+]
+export const tours = [
+    {
+        id:1,
+        img: tour1,
+        date: 'august 26th, 2020',
+        title: 'Tibet Adventure',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        location: 'china',
+        icon: 'fas fa-map',
+        duration: '6 days',
+        price: 'from $2100',
+    },
+    {
+        id:2,
+        img: tour2,
+        date: 'october 1st, 2020',
+        title: 'best of java',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        location: 'indonesia',
+        icon: 'fas fa-map',
+        duration: '11 days',
+        price: 'from $1400',
+    },
+    {
+        id:3,
+        img: tour3,
+        date: 'september 15th, 2020',
+        title: 'explore hong kong',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        location: 'hong kong',
+        icon: 'fas fa-map',
+        duration: '8 days',
+        price: 'from $5000',
+    },
+    {
+        id:4,
+        img: tour4,
+        date: 'december 5th, 2019',
+        title: 'kenya highlights',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        location: 'kenya',
+        icon: 'fas fa-map',
+        duration: '20 days',
+        price: 'from $3300',
+    },
+    {
+        id:5,
+        img: tour5,
+        date: 'may 31st, 2019',
+        title: 'zebra discovery',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        location: 'ethiopia',
+        icon: 'fas fa-map',
+        duration: '16 days',
+        price: 'from $3600',
+    },
+    {
+        id:6,
+        img: tour6,
+        date: 'april 12th, 2019',
+        title: 'journey to Taj Mahal',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        location: 'india',
+        icon: 'fas fa-map',
+        duration: '40 days',
+        price: 'from $7300',
+    },
+
+]
